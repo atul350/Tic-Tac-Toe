a5f6adcb35a9f22b53c82291a87922069e4eb574
atul350 <atulrajput312006@gmail.com>
2025-11-24 15:03:07 +0530

    Add files via upload

--- project.c
+++ project.c
@@ -1,88 +1,125 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>   // For getch() and kbhit() (Windows)
+#include <windows.h> // For Sleep()
 
-char board[3][3] = {
-    {'1','2','3'},
-    {'4','5','6'},
-    {'7','8','9'}
-};
+#define WIDTH 40
+#define HEIGHT 20
 
-void displayBoard() {
-    printf("\n");
-    printf(" %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
-    printf("---+---+---\n");
-    printf(" %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
-    printf("---+---+---\n");
-    printf(" %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
-    printf("\n");
+int gameOver;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int tailLength;
+char direction;
+
+void setup() {
+    gameOver = 0;
+    direction = ' ';
+    x = WIDTH / 2;
+    y = HEIGHT / 2;
+    fruitX = rand() % WIDTH;
+    fruitY = rand() % HEIGHT;
+    score = 0;
 }
 
-int checkWinner() {
-    // Rows
-    for(int i = 0; i < 3; i++)
-        if(board[i][0] == board[i][1] && board[i][1] == board[i][2]) 
-            return 1;
+void draw() {
+    system("cls"); // Clear screen
 
-    // Columns
-    for(int i = 0; i < 3; i++)
-        if(board[0][i] == board[1][i] && board[1][i] == board[2][i])
-            return 1;
+    for (int i = 0; i < WIDTH + 2; i++) printf("#");
+    printf("\n");
 
-    // Diagonals
-    if(board[0][0] == board[1][1] && board[1][1] == board[2][2])
-        return 1;
-    if(board[0][2] == board[1][1] && board[1][1] == board[2][0])
-        return 1;
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
 
-    return 0;
+            if (j == 0) printf("#");
+
+            if (i == y && j == x)
+                printf("O"); // Snake head
+            else if (i == fruitY && j == fruitX)
+                printf("F"); // Fruit
+            else {
+                int printTail = 0;
+                for (int k = 0; k < tailLength; k++)
+                    if (tailX[k] == j && tailY[k] == i) {
+                        printf("o");
+                        printTail = 1;
+                    }
+
+                if (!printTail) printf(" ");
+            }
+
+            if (j == WIDTH - 1) printf("#");
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < WIDTH + 2; i++) printf("#");
+    printf("\n");
+
+    printf("Score: %d\n", score);
 }
 
-int isDraw() {
-    for(int i = 0; i < 3; i++)
-        for(int j = 0; j < 3; j++)
-            if(board[i][j] != 'X' && board[i][j] != 'O')
-                return 0;
-    return 1;
+void input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+            case 'w': direction = 'U'; break;
+            case 's': direction = 'D'; break;
+            case 'a': direction = 'L'; break;
+            case 'd': direction = 'R'; break;
+            case 'x': gameOver = 1; break;
+        }
+    }
+}
+
+void logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < tailLength; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (direction) {
+        case 'U': y--; break;
+        case 'D': y++; break;
+        case 'L': x--; break;
+        case 'R': x++; break;
+    }
+
+    if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
+        gameOver = 1;
+
+    for (int i = 0; i < tailLength; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = 1;
+
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        fruitX = rand() % WIDTH;
+        fruitY = rand() % HEIGHT;
+        tailLength++;
+    }
 }
 
 int main() {
-    int choice, row, col;
-    char mark;
-    int player = 1;
+    setup();
 
-    while(1) {
-        displayBoard();
-        player = (player % 2) ? 1 : 2;
-        mark = (player == 1) ? 'X' : 'O';
-
-        printf("Player %d, enter choice: ", player);
-        scanf("%d", &choice);
-
-        // Convert choice to row & col
-        row = (choice - 1) / 3;
-        col = (choice - 1) % 3;
-
-        // Validate move
-        if(choice < 1 || choice > 9 || board[row][col] == 'X' || board[row][col] == 'O') {
-            printf("Invalid move! Try again.\n");
-            continue;
-        }
-
-        board[row][col] = mark;
-
-        if(checkWinner()) {
-            displayBoard();
-            printf("Player %d wins!\n", player);
-            break;
-        }
-
-        if(isDraw()) {
-            displayBoard();
-            printf("It's a draw!\n");
-            break;
-        }
-
-        player++;
+    while (!gameOver) {
+        draw();
+        input();
+        logic();
+        Sleep(80); // Speed control
     }
 
+    printf("\nGAME OVER! Final Score = %d\n", score);
     return 0;
 }
